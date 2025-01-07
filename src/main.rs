43db504873f5a157d5c63d821df3c760c5acb56f
FoLZer <65248062+FoLZer@users.noreply.github.com>
2025-01-07 19:55:51 +0100

    License header

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+/*
+barotrauma_conflict_finder
+Copyright (C) 2025  FoLZer
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 pub mod content_file;
 pub mod content_package;
 pub mod loading;
